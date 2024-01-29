9e5363b29c50645a8c72af47c8d15fd691b18bbd
Kamil <Str4ngerGameOne@gmail.com>
2024-01-29 14:50:40 +0100

    feat: Layer extensions.

--- src/MiniUrl.Api/Program.cs
+++ src/MiniUrl.Api/Program.cs
@@ -1,4 +1,14 @@
+using MiniUrl.Application;
+using MiniUrl.Domain;
+using MiniUrl.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddDomain()
+    .AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
