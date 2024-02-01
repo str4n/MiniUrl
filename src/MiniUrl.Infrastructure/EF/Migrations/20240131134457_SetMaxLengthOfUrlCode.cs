70a3c2ba512c66feffc2693e50c35a1f560d75f4
Kamil <Str4ngerGameOne@gmail.com>
2024-02-01 18:35:31 +0100

    fix: Url code length.

---
+++ src/MiniUrl.Infrastructure/EF/Migrations/20240131134457_SetMaxLengthOfUrlCode.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniUrl.Infrastructure.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetMaxLengthOfUrlCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                schema: "url",
+                table: "ShortenedUrls",
+                type: "character varying(6)",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                schema: "url",
+                table: "ShortenedUrls",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(6)",
+                oldMaxLength: 6);
+        }
+    }
+}
