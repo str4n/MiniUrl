4d4b770c619d57a210af136132aab09513b02cea
Kamil <Str4ngerGameOne@gmail.com>
2024-02-07 16:41:25 +0100

    feat: Code generator unit tests.

---
+++ tests/MiniUrl.Tests.Unit/Helpers/Repositories/InMemoryUrlRepository.cs
@@ -0,0 +1,24 @@
+﻿using MiniUrl.Domain.Repositories;
+using MiniUrl.Domain.Url;
+
+namespace MiniUrl.Tests.Unit.Helpers.Repositories;
+
+internal sealed class InMemoryUrlRepository : IUrlRepository
+{
+    private static readonly List<ShortenedUrl> ShortenedUrls = new()
+    {
+        new ShortenedUrl("https://www.youtube.com/", "https://localhost:9990/youtube", "youtube", DateTime.Now, DateTime.MaxValue)
+    };
+
+    public Task<ShortenedUrl> GetAsync(Code code)
+        => Task.FromResult(ShortenedUrls.SingleOrDefault(x => x.Code == code));
+
+    public Task AddAsync(ShortenedUrl shortenedUrl)
+    {
+        ShortenedUrls.Add(shortenedUrl);
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> AnyAsync(Code code)
+        => Task.FromResult(ShortenedUrls.Any(x => x.Code == code));
+}
---
+++ tests/MiniUrl.Tests.Unit/Services/UrlCodeGeneratorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using MiniUrl.Application.Services;
+using MiniUrl.Domain.Repositories;
+using MiniUrl.Domain.Url;
+using MiniUrl.Tests.Unit.Helpers.Repositories;
+using Xunit;
+
+namespace MiniUrl.Tests.Unit.Services;
+
+public class UrlCodeGeneratorTests
+{
+    [Fact]
+    public async Task Generate_Method_Should_Return_Proper_Url_Code()
+    {
+        var code = await _codeGenerator.Generate();
+
+        code.Value.Should().HaveLength(7);
+        code.Value.Select(x => x.Should().BeOneOf(ShortUrlSettings.AvailableCharacters.ToCharArray()));
+    }
+
+    private readonly IUrlCodeGenerator _codeGenerator;
+
+    public UrlCodeGeneratorTests()
+    {
+        IUrlRepository urlRepository = new InMemoryUrlRepository();
+        _codeGenerator = new UrlCodeGenerator(urlRepository);
+    }
+}
