c4279093f9d85bd225fc07b7f0b17d9601b03315
Kamil <Str4ngerGameOne@gmail.com>
2024-02-08 15:11:17 +0100

    feat: PermanentLifeTimeStrategy unit tests.

---
+++ tests/MiniUrl.Tests.Unit/Strategies/PermanentLifeTimeStrategyTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using MiniUrl.Application.Exceptions;
+using MiniUrl.Application.Requests;
+using MiniUrl.Application.Strategies;
+using MiniUrl.Domain.Url;
+using Xunit;
+
+namespace MiniUrl.Tests.Unit.Strategies;
+
+public class PermanentLifeTimeStrategyTests : ShorteningStrategyTests
+{
+    [Fact]
+    public async Task Given_Valid_Request_Strategy_Should_Shorten_Url_In_Proper_Way()
+    {
+        const string url = "https://www.youtube.com/";
+        const string schema = "https";
+        const string host = "localhost:9990";
+        const string code = "youtub";
+        
+        var request = new ShortenUrlRequest(schema, host, 
+            url, code, default);
+
+        var result = await _shorteningStrategy.ShortenUrl(request);
+        
+        var expectedShortUrl = (Url)$"{schema}://{host}/{code}";
+        
+        result.LongUrl.Should().Be(url);
+        result.ShortUrl.Should().Be(expectedShortUrl);
+
+        var shortenedUrl = await  Repository.GetAsync(code);
+
+        shortenedUrl.Code.Should().Be((Code)code);
+        shortenedUrl.ShortUrl.Should().Be(expectedShortUrl);
+        shortenedUrl.LongUrl.Should().Be((Url)url);
+        shortenedUrl.CreatedAt.Should().Be(Clock.Now());
+        shortenedUrl.Expiry.Should().Be(DateTime.MaxValue);
+    }
+    
+    [Fact]
+    public async Task Given_Request_With_Code_Already_In_Use_Strategy_Should_Shorten_Url_In_Proper_Way()
+    {
+        const string url = "https://www.youtube.com/";
+        const string schema = "https";
+        const string host = "localhost:9990";
+        const string code = "youtube";
+        
+        var request = new ShortenUrlRequest(schema, host, 
+            url, code, default);
+
+        var act = () => _shorteningStrategy.ShortenUrl(request);
+        
+        await act.Should().ThrowAsync<CustomCodeAlreadyExistsException>();
+    }
+    
+    private readonly IShorteningStrategy _shorteningStrategy;
+    
+    public PermanentLifeTimeStrategyTests()
+    {
+        _shorteningStrategy = new PermanentLifeTimeStrategy(Repository, Clock);
+    }
+}
