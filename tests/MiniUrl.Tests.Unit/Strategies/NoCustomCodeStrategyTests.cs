0be384decfde5bebeb43695d65cd6d3646abf530
Kamil <Str4ngerGameOne@gmail.com>
2024-02-08 14:59:34 +0100

    feat: Separated strategy test dependencies to abstract class.

---
+++ tests/MiniUrl.Tests.Unit/Strategies/NoCustomCodeStrategyTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using MiniUrl.Application.Requests;
+using MiniUrl.Application.Strategies;
+using MiniUrl.Domain.Url;
+using Xunit;
+
+namespace MiniUrl.Tests.Unit.Strategies;
+
+public class NoCustomCodeStrategyTests : ShorteningStrategyTests
+{
+    [Fact]
+    public async Task Given_Valid_Request_Strategy_Should_Shorten_Url_In_Proper_Way()
+    {
+        const string url = "https://www.youtube.com/";
+        const string schema = "https";
+        const string host = "localhost:9990";
+        const int lifeTime = 12;
+        
+        var request = new ShortenUrlRequest(schema, host, 
+            url, null, lifeTime);
+
+        var result = await _shorteningStrategy.ShortenUrl(request);
+        
+        var code = result.ShortUrl.Replace($"{schema}://{host}/", string.Empty);
+        var expectedShortUrl = (Url)$"{schema}://{host}/{code}";
+
+        result.LongUrl.Should().Be(url);
+        result.ShortUrl.Should().Be(expectedShortUrl);
+
+        var shortenedUrl = await  Repository.GetAsync(code);
+
+        shortenedUrl.Code.Should().Be((Code)code);
+        shortenedUrl.ShortUrl.Should().Be(expectedShortUrl);
+        shortenedUrl.LongUrl.Should().Be((Url)url);
+        shortenedUrl.CreatedAt.Should().Be(Clock.Now());
+        shortenedUrl.Expiry.Should().Be(Clock.Now().AddHours(lifeTime));
+    }
+
+    private readonly IShorteningStrategy _shorteningStrategy;
+
+    public NoCustomCodeStrategyTests()
+    {
+        _shorteningStrategy = new NoCustomCodeStrategy(Repository, Clock, CodeGenerator);
+    }
+}
---
+++ tests/MiniUrl.Tests.Unit/Strategies/ShorteningStrategyTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using MiniUrl.Application.Requests;
+using MiniUrl.Application.Services;
+using MiniUrl.Application.Strategies;
+using MiniUrl.Domain.Repositories;
+using MiniUrl.Domain.Url;
+using MiniUrl.Infrastructure.EF.Repositories;
+using MiniUrl.Infrastructure.Time;
+using MiniUrl.Tests.Unit.Helpers.Clock;
+using MiniUrl.Tests.Unit.Helpers.Repositories;
+using Xunit;
+
+namespace MiniUrl.Tests.Unit.Strategies;
+
+public abstract class ShorteningStrategyTests
+{
+    protected readonly IUrlRepository Repository;
+    protected readonly IClock Clock;
+    protected readonly IUrlCodeGenerator CodeGenerator;
+
+    public ShorteningStrategyTests()
+    {
+        var clock = new TestClock();
+        var repository = new InMemoryUrlRepository();
+        CodeGenerator = new UrlCodeGenerator(repository);
+        Repository = repository;
+        Clock = clock;
+    }
+}
