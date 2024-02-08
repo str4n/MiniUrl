4be43cb9817cae5df744ba286cd46a1d214e0ddd
Kamil <Str4ngerGameOne@gmail.com>
2024-02-08 14:41:24 +0100

    feat: NoCustomCodeStrategy unit test.

---
+++ tests/MiniUrl.Tests.Unit/Helpers/Clock/TestClock.cs
@@ -0,0 +1,8 @@
+﻿using MiniUrl.Infrastructure.Time;
+
+namespace MiniUrl.Tests.Unit.Helpers.Clock;
+
+public class TestClock : IClock
+{
+    public DateTime Now() => DateTime.Parse("08/02/2024");
+}
